6a9e6c0b714f03efd5b692f3dbfd6d357f3687c8
louiselumapas <louise.lumapas.ll@gmail.com>
2019-01-25 12:02:09 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get  '/help', to: 'static_pages#help'   # request for the URL /help to the help action in the Static Pages controller
   get  '/about', to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  	'/signup', to: 'users#new'
+  get  '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :users #automatically ensures that our Rails application responds to the RESTful URLs
 end
