35f63d6da99549ba6ee8f268e1606b34afee467d
louiselumapas <louise.lumapas.ll@gmail.com>
2019-01-25 10:46:02 +0800

    initial sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
-	attr_accessor :name, :email
-  def new
+  attr_accessor :name, :email
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   get 'users/new'
-
   root 'static_pages#home'
   get  '/help', to: 'static_pages#help'   # request for the URL /help to the help action in the Static Pages controller
   get  '/about', to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  	'/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users #automatically ensures that our Rails application responds to the RESTful URLs
 end
